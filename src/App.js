423550c6306208844914bb91db263e2800792c1a
ps-lolfan <pbsinnur@gmail.com>
2021-06-15 10:48:01 +0530

    map added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import React, { useEffect, useState, useRef } from 'react';
 import './App.css';
+import * as tt from '@tomtom-international/web-sdk-maps'
 
-function App() {
+const App = () => {
+  const mapElement = useRef()
+  const [map, setMap] = useState({})
+
+  useEffect(() => {
+    let map = tt.map({
+      key: process.env.REACT_APP_MAP_KEY,
+      container: mapElement.current,
+    });
+
+    setMap(map)
+  }, [])
+
   return (
     <div className="App">
-
+      <div ref={mapElement}></div>
     </div>
   );
 }
